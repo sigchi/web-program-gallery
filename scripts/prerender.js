b184566b442be7bb28879e0dae11d4997e373770
John Sullivan <jsullivan@csumb.edu>
2021-02-08 02:37:32 -0800

    example with water-css

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -2,13 +2,15 @@ import { promises as fs } from "fs";
 import Gallery from "../src/components/Gallery.svelte";
 import program from "../build/js/program.js";
 
-const page = "./build/index.html";
+const pages = ["./build/index.html", "./build/index-water.html"];
 const { head, html } = Gallery.render({ program });
 
-fs.readFile(page, "utf8").then((text) => {
-  const final = text
-    .replace("<!-- svelte.head -->", head)
-    .replace("<!-- svelte.html -->", html);
+pages.forEach((page) =>
+  fs.readFile(page, "utf8").then((text) => {
+    const final = text
+      .replace("<!-- svelte.head -->", head)
+      .replace("<!-- svelte.html -->", html);
 
-  return fs.writeFile(page, final, "utf8");
-});
+    return fs.writeFile(page, final, "utf8");
+  })
+);
