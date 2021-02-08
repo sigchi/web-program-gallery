66d028319f3ab1c78b5a2dbabcc931b71c973f23
John Sullivan <jsullivan@csumb.edu>
2021-02-08 02:32:50 -0800

    labels for vid thumbnails

--- src/components/VidThumb.svelte
+++ src/components/VidThumb.svelte
@@ -16,16 +16,61 @@
 {#if thumbImg !== undefined}
   <a
     href={url}
+    aria-label={name}
     on:click={(e) => {
       e.preventDefault();
       watch();
-    }}><img alt={name} src={thumbImg} /></a
+    }}
   >
+    <figure>
+      <img alt={name} src={thumbImg} />
+      <figcaption>
+        <h1>⏵</h1>
+        <span>{name}</span>
+      </figcaption>
+    </figure>
+  </a>
 {/if}
 
 <style>
   a {
     display: inline-block;
     max-width: var(--sigchi-article-thumbnail-width);
+  }
+
+  figure {
+    position: relative;
+    width: 100%;
+    margin: 0;
+  }
+
+  figure * {
+    margin: 0;
+  }
+
+  figcaption {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  figcaption * {
+    color: black;
+    background-color: #ebebebbb;
+  }
+
+  figcaption h1 {
+    color: white;
+    background-color: #00000088;
+    text-align: center;
+    width: 45%;
+    border-radius: 15px;
   }
 </style>
